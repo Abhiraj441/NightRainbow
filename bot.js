d2d95984ef7fe52c61aa93030211e66fc25a11c6
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-03-31 12:07:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,8 @@ bot.on('message', message => {
 
     }
     if(command === settings.prefix + settings.rainbowstop) {
-            setTimeout(function () {
+            if(!message.guild.member(message.author.id).hasPermission("ADMINISTRATOR")) return message.channel.send(settings.messageresponse.membernoperm).catch(err=> message.channel.send("no response"))
+        setTimeout(function () {
            process.exit()
             }, 1000);
            
