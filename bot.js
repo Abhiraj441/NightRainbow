813b5e72309514ebdd62e13554fb398453a06c27
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-01 16:37:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,4 +31,32 @@ bot.on('message', message => {
                        message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
                        }
 });
+bot.on('message', message => {
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+    if(command === settings.prefix + settings.rgbcommand) {
+        const delay = args.shift().toLowerCase();
+        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
+        if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
+        if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
+        if(!message.guild.member(message.author.id).hasPermission("ADMINISTRATOR")) return message.channel.send(settings.messageresponse.membernoperm).catch(err=> message.channel.send("no response"))
+        if(delay < 1400) return message.reply('Please input a number higher than 1400.')
+        var colors = settings.rainbowrole
+        var rolestart = setInterval(function() {
+            var colorsz = colors[Math.floor(Math.random() * colors.length)];
+            rolez.setColor(colorsz)
+        }, delay); 
+            message.channel.send(settings.messageresponse.rgbstart).catch(err=> message.channel.send("No response"))
+
+    }
+    if(command === settings.prefix + settings.rainbowstop) {
+            if(!message.guild.member(message.author.id).hasPermission("ADMINISTRATOR")) return message.channel.send(settings.messageresponse.membernoperm).catch(err=> message.channel.send("no response"))
+            setTimeout(function () {
+           process.exit()
+            }, 1000);
+           
+                       message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
+                       }
+});
 bot.login(settings.token).catch(err=> console.log("Incorrect Token was provided"))
