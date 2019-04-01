55e28c432c8a4a468b9a6bd4d2c159b6ab734191
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-01 14:14:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ bot.on('message', message => {
     let command = messageArray[0];
     let args = messageArray.slice(1);
     if(command === settings.prefix + settings.rainbowcommand) {
+        const delay = args.shift().toLowerCase();
         const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
         if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
         if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
@@ -17,7 +18,7 @@ bot.on('message', message => {
         var rolestart = setInterval(function() {
             var colorsz = colors[Math.floor(Math.random() * colors.length)];
             rolez.setColor(colorsz)
-        }, settings.rainbowdelay); 
+        }, delay); 
             message.channel.send(settings.messageresponse.success).catch(err=> message.channel.send("No response"))
 
     }
