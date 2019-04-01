85cdadc053258b0e4e72a4ed18bf15923280dd8d
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-01 17:31:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,8 +118,9 @@ bot.on('message', message => {
 });
 bot.on('message', message => {
     if(command === settings.prefix + settings.helpcommand) {
-        let embed = new Discord.RichEmbed()   
-.setAuthor(client.user.username, client.user.avatarURL)
+        let embed = new Discord.RichEmbed()  
+        
+.setAuthor(bot.user.username, bot.user.avatarURL)
 .setColor("#DCA741")
 .setDescription("Here you can see the commands for " + client.user.username + "!")
 .addField(` ^color time @rolename - This command apply a rainbow effect on the specified role"`)
