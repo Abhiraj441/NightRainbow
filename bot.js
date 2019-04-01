344476e889d3cc3b54a8f5e0146991cd8d379137
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-01 09:33:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,59 @@ bot.on('message', message => {
            process.exit()
             }, 1000);
            
-                       message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
+                       message.channel.send(settings.messageresponse.rgbstop).catch(err=> message.channel.send("No response"))
+                       }
+});
+bot.on('message', message => {
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+    if(command === settings.prefix + settings.rocommand) {
+        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
+        if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
+        if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
+        if(!message.guild.member(message.author.id).hasPermission("ADMINISTRATOR")) return message.channel.send(settings.messageresponse.membernoperm).catch(err=> message.channel.send("no response"))
+        var colors = settings.ro
+        var rolestart = setInterval(function() {
+            var colorsz = colors[Math.floor(Math.random() * colors.length)];
+            rolez.setColor(colorsz)
+        }, settings.rainbowdelay); 
+            message.channel.send(settings.messageresponse.fstart).catch(err=> message.channel.send("No response"))
+
+    }
+    if(command === settings.prefix + settings.rainbowstop) {
+            if(!message.guild.member(message.author.id).hasPermission("ADMINISTRATOR")) return message.channel.send(settings.messageresponse.membernoperm).catch(err=> message.channel.send("no response"))
+        setTimeout(function () {
+           process.exit()
+            }, 1000);
+           
+                       message.channel.send(settings.messageresponse.fstop).catch(err=> message.channel.send("No response"))
+                       }
+});
+bot.on('message', message => {
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+    if(command === settings.prefix + settings.gmcommand) {
+        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
+        if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
+        if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
+        if(!message.guild.member(message.author.id).hasPermission("ADMINISTRATOR")) return message.channel.send(settings.messageresponse.membernoperm).catch(err=> message.channel.send("no response"))
+        var colors = settings.gm
+        var rolestart = setInterval(function() {
+            var colorsz = colors[Math.floor(Math.random() * colors.length)];
+            rolez.setColor(colorsz)
+        }, settings.rainbowdelay); 
+            message.channel.send(settings.messageresponse.fstart).catch(err=> message.channel.send("No response"))
+
+    }
+    if(command === settings.prefix + settings.rainbowstop) {
+            if(!message.guild.member(message.author.id).hasPermission("ADMINISTRATOR")) return message.channel.send(settings.messageresponse.membernoperm).catch(err=> message.channel.send("no response"))
+        setTimeout(function () {
+           process.exit()
+            }, 1000);
+           
+                       message.channel.send(settings.messageresponse.fstop).catch(err=> message.channel.send("No response"))
                        }
 });
 bot.login(settings.token).catch(err=> console.log("Incorrect Token was provided"))
