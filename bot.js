5d5d37c72122d25044031850ff846b22e49b8abe
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-02 20:03:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,4 +101,23 @@ bot.on('message', message => {
 message.channel.send(embed);
     }
 });
+
+bot.on('message', message => {
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+    if(command === settings.prefix + settings.trollcommand) {
+        const delay = args.shift().toLowerCase();
+        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
+        if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
+        if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
+        if(!message.guild.member(message.author.id).hasPermission("ADMINISTRATOR")) return message.channel.send(settings.messageresponse.membernoperm).catch(err=> message.channel.send("no response"))
+        if(delay < 100) return message.reply('Please input a number higher than 100.(Because that is a troll command')
+        var colors = settings.troll
+        var rolestart = setInterval(function() {
+            var colorsz = colors[Math.floor(Math.random() * colors.length)];
+            rolez.setColor(colorsz)
+        }, delay); 
+            message.channel.send(settings.messageresponse.trollstart).catch(err=> message.channel.send("No response"))
+});
 bot.login(settings.token).catch(err=> console.log("Incorrect Token was provided"))
