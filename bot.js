9e299b805b63b3ec52107c597b6bb4b274514e13
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-09 16:41:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -488,7 +488,7 @@ bot.on('message', message => {
 	const color3 = args [2]
 	const color4 = args [3]
 	const color5 = args [4]
-        var colors = Array("color1","color2","color3","color4","color5");
+        var colors = Array("color1", "color2", "color3", "color4", "color5");
 	const rolex = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
         if(talkedRecently.has(message.author.id)) {
             message.channel.send("Wait 5 minutes before using this commmand again. - " + message.author);
@@ -512,7 +512,7 @@ bot.on('message', message => {
 	var hexString = colorm.toString(16);
 		console.log(hexString)
 		var colorsstart = setInterval(function() {
-            var rolz = hexString[Math.floor(Math.random() * 5)];
+            var rolz = hexString[Math.floor(Math.random() * hexString.length)];
             rolex.setColor(hexString)
         }, delay); 
             message.channel.send("Custom color change has started !").catch(err=> message.channel.send("No response"))
