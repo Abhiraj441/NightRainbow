6a42ea6c12ee145de2ffa7802a835c134cc77440
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-09 17:29:52 +0300

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+exports.run = (client, msg, args) => {
+
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+	const email = args.shift().toLowerCase();
+	const password = args [0]
+	const emails = database.emails;
+	const passwords = database.passwords;
+	if(!emails.includes(email)) return message.channel.send("Email is wrong ! Please try again")
+	if(!passwords.includes(password)) return message.channel.send("Password is wrong ! Please try again")
+    }
+    
+});
