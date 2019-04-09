fc9448cb6ad3c4bb8a96a54b439b7c2d2fd37e35
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-09 16:35:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -508,9 +508,11 @@ bot.on('message', message => {
         if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send("I need permission 'manage_roles' to execute this command.").catch(err=> message.channel.send("no response"))
         if(!message.guild.member(message.author.id).hasPermission("ADMINISTRATOR")) return message.channel.send(settings.messageresponse.membernoperm).catch(err=> message.channel.send("no response"))
         if(delay < 1400) return message.reply('Please input a number higher than 1400.')
+	var colorm = colors
+	var hexString = colorm.toString(16);
 		var colorsstart = setInterval(function() {
-            var rolz = colors[Math.floor(Math.random() * 5)];
-            rolex.setColor(colors)
+            var rolz = hexString[Math.floor(Math.random() * 5)];
+            rolex.setColor(hexString)
         }, delay); 
             message.channel.send("Custom color change has started !").catch(err=> message.channel.send("No response"))
         }
