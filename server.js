f26aef942fdfc0ef3d66561d6c8d8b88545b03df
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-18 09:07:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ const members = database.members;
 	let args = messageArray.slice(1);
            if(command === `^reload`) {
 		   if(!owner.includes(message.author.id)) return message.reply("You are not my owner !")
-									       const heroku = require("heroku-client");
-		   heroku ps:restart
+		   var http = require("http");
+setInterval(function() {
+    http.get("http://discordrainbowwas.herokuapp.com");
+}, 300000);
 			   message.channel.send("Reloaded !")
         }
         if(command === settings.prefix + settings.rainbowcommand) {      
