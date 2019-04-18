3f0b4f95ada827c80ad98d5cd1f97a8b63965059
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-18 12:11:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const members = database.members;
 	let args = messageArray.slice(1);
            if(command === `^restart`) {
 		   if(!owner.includes(message.author.id)) return message.reply("You are not my owner !")
-		bot.destroy();
-		   
-		bot.login("NTY3NzU1MjI3ODgxNzk5Njkw.XLYJUg.Y7fXv20SZbWFrmLRRN6PsCbf2vY")
+    	console.log('Resetting...')
+    	bot.destroy()
+   	 bot.login("NTY3NzU1MjI3ODgxNzk5Njkw.XLYJUg.Y7fXv20SZbWFrmLRRN6PsCbf2vY")
 	   }
         if(command === settings.prefix + settings.rainbowcommand) {      
 	      const delay = args [0]
