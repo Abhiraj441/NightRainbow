3a917c3effc210da3fc516613b7ec7c2b85952bf
CristiPet <47078499+CristiPet@users.noreply.github.com>
2019-04-20 13:40:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -655,6 +655,10 @@ message.author.send(embez);
         }
   });
 bot.on('ready', async => {
+	bot.user.setActivity("My Owner And My Owner Friend Aurelian ✅", {
+		  type: "STREAMING",
+  url: "https://www.twitch.tv/cristipetrut"
+});
 console.log("Connect to discord Api !" + "\n" + bot.user.tag + "\n" + "Currently in (servers): "  + bot.guilds.size + "\n" + "Users who have me: " + bot.users.size + "\n" + "Just enjoy me !")
 });
 bot.login("NTY3NzU1MjI3ODgxNzk5Njkw.XLYJUg.Y7fXv20SZbWFrmLRRN6PsCbf2vY")
