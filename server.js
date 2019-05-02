c0e7c80fd6d9bfdea5e934214f6fcbdb8f830af1
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-05-02 18:57:32 +0600

    Update server.js

--- server.js
+++ server.js
@@ -690,8 +690,7 @@ message.author.send(embez);
         }
     };
 bot.on('ready', async => {
-	bot.user.setActivity("GhostRider Premium plus"), {		  type: "STREAMING",
-  url: "https://www.twitch.tv/고스트 라이더 Aka Ayush"});
+	bot.user.setActivity("GhostRider Premium plus"), {		  type: "STREAMING",url: "https://www.twitch.tv/고스트 라이더 Aka Ayush");
 console.log("Connect to discord Api !" + "\n" + bot.user.tag + "\n" + "Currently in (servers): "  + bot.guilds.size + "\n" + "Users who have me: " + bot.users.size + "\n" + "Just enjoy me !")
 });
 bot.login(process.env.token)
